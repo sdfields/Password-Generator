592d1cbdf01b79966131d0de19ecce0422e72a25
sdfields <spencerdfields@gmail.com>
2022-11-04 13:22:40 -0500

    initial starter code with potential solutions

---
+++ script.js
@@ -0,0 +1,110 @@
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// Potential Generators
+// function getRandomLowerCase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function getRandomUpperCase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+
+// function getRandomSymbol() {
+//   const symbols = "!@#$%^&*()[]{}=<>/,."
+//   return String.fromCharCode(Math.floor(Math.random() * symbols.length));
+// }
+
+// console.log(getRandomLowerCase);
+
+
+
+// # 03 JavaScript: Password Generator
+
+// ## Your Task
+
+// This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+// ## Acceptance Criteria
+
+// ```
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// ```
+
+// ## Mock-Up
+
+// The following image shows the web application's appearance and functionality:
+
+// ![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+
+// This Challenge is graded based on the following criteria: 
+
+// ### Technical Acceptance Criteria: 40%
+
+// * Satisfies all of the preceding acceptance criteria.
+
+// ### Deployment: 32%
+
+// * Application deployed at live URL.
+
+// * Application loads with no errors.
+
+// * Application GitHub URL submitted.
+
+// * GitHub repository that contains application code.
+
+// ### Application Quality: 15%
+
+// * Application user experience is intuitive and easy to navigate.
+
+// * Application user interface style is clean and polished.
+
+// * Application resembles the mock-up functionality provided in the Challenge instructions.
+
+// ### Repository Quality: 13%
+
+// * Repository has a unique name.
+
+// * Repository follows best practices for file structure and naming conventions.
+
+// * Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+
+// * Repository contains multiple descriptive commit messages.
+
+// * Repository contains quality readme file with description, screenshot, and link to deployed application.
