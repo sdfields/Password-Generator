96a4d2bba34eb3b75e2b5a02107da15d5aafefca
sdfields <spencerdfields@gmail.com>
2022-11-07 23:11:36 -0600

    created global variables and functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,101 @@
 // Assignment Code
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
+var criteria = {
+  passwordLength: 0,
+  lowerCase: true,
+  upperCase: true,
+  number: true,
+  symbol: true,
+}
 
 var generateBtn = document.querySelector("#generate");
+
+var generatePassword = function() {
+    var passwordArray = [];
+    var finalPassword = [];
+  
+  if (criteria.lowerCase) {
+    passwordArray = passwordArray.concat(lowerCase);
+  }
+  if (criteria.upperCase) {
+    passwordArray = passwordArray.concat(upperCase);
+  }
+  if (criteria.number) {
+    passwordArray = passwordArray.concat(number);
+  }
+  if (criteria.symbol) {
+    passwordArray = passwordArray.concat(symbol);
+  }
+
+  for (let index = 0; index < criteria.passwordLength; index++) {
+    finalPassword.push(passwordArray[Math.floor(Math.random() * passwordArray.length)])
+  }
+ 
+  return finalPassword.join("")
+
+}
+
+
 
 // Write password to the #password input
 
 function writePassword() {
+  var passwordLength = function () {
+    var l = window.prompt("How many characters would you like to use?")
+    if (l >= 8 && l <= 128) {
+      criteria.passwordLength = l
+      }  else if (l == null) {
+        return;
+      } else {
+        window.alert("Your password does not meet the criteria.")
+        passwordLength()
+        return;
+      }
+  }
+
+  passwordLength()
+
+  // Lower Case
+
+  var lowerCase = function () {
+    var lc = window.confirm("Do you want to use lowercase letters?")
+      criteria.lowerCase = lc
+  }
+
+  lowerCase()
+
+  // Upper Case
+
+  var upperCase = function () {
+    var uc = window.confirm("Do you want to use uppercase letters?")
+      criteria.upperCase = uc
+  }
+
+  upperCase()
+
+  // Number 
+
+  var number = function () {
+    var n = window.confirm("Do you want to use numbers?")
+      criteria.number = n
+  }
+
+  number()
+
+  // Symbol
+
+  var symbol = function () {
+    var s = window.confirm("Do you want to use symbols?")
+      criteria.symbol = s
+  }
+
+  symbol()
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -16,28 +107,6 @@ function writePassword() {
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// Potential Generators
-// function getRandomLowerCase() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRandomUpperCase() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function getRandomSymbol() {
-//   const symbols = "!@#$%^&*()[]{}=<>/,."
-//   return String.fromCharCode(Math.floor(Math.random() * symbols.length));
-// }
-
-// console.log(getRandomLowerCase);
 
 
 
